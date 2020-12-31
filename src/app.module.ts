7472f71407c81f6ad86ba62eb02ac1e758069b09
chg1213 <chg1213@naver.com>
2020-12-31 19:38:37 +0900

    1.2 services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 // 모듈의 모든 루트와 같은 것
 @Module({
   imports: [],
-  controllers: [AppController], // url을 가져오고 함수를 실행하는 것,express의 라우터와 같은 것
-  providers: [AppService],
+  controllers: [], // url을 가져오고 함수를 실행하는 것,express의 라우터와 같은 것
+  providers: [],
 })
 export class AppModule { }
